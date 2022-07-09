ff3906b884914f401b0a4710f806e55aab3d1ee5
Ernesto Pestaña <epestanamx@gmail.com>
2022-07-09 14:36:16 -0500

    Configuración inicial de express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hola mundo');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.status(200).send({
+    name: 'wordle-backend',
+    version: '1.0.0',
+    description: 'Wordle Backend',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listen on port', 3000);
+});
